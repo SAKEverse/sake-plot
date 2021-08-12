71812650a8eb47da327d271657ff81ac8fe46b64
Pantelis Antonoudiou <pantelis71@hotmail.com>
2021-08-12 14:49:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import os
 import yaml
 import click
 from pick import pick 
+from app_interface import file_present_check
 ########## ---------------------------------------------------------------- ##########
 
 
@@ -18,19 +19,37 @@ from pick import pick
 def main(ctx):
     """
 
-    ██████  ██    ██ ██████  ███████ ██████                                     
+    ███████ ██    ██ ██████  ███████ ███████                                     
     ██   ██  ██  ██  ██   ██ ██      ██   ██                                      
-    ██████    ████   ██   ██ ███████ ██████                                                                            
+    ███████   ████   ██   ██ ███████ ███████                                                                            
     ██         ██    ██   ██      ██ ██                                           
     ██         ██    ██████  ███████ ██                                           
              
      
     """
     
-    # read file
+    # get settings and pass to context
     with open(settings_path, 'r') as file:
         settings = yaml.load(file, Loader=yaml.FullLoader)
-        ctx.obj.update({'settings':settings})
+        ctx.obj = settings.copy()
+        
+    # save original settings
+    ctx.obj.update({'settings': settings}) 
+        
+    # set files present to zero
+    ctx.obj.update({'index_present': 0})
+    ctx.obj.update({'power_present': 0})
+    
+    # check if index file is present
+    ctx.obj.update({'index_path': os.path.join(ctx.obj['search_path'], ctx.obj['file_index'])})
+    if os.path.isfile(ctx.obj['index_path']):
+        ctx.obj.update({'index_present':1})
+        
+    # check if power mat file is present
+    ctx.obj.update({'power_mat_path': os.path.join(ctx.obj['search_path'], ctx.obj['file_power_mat'])})
+    if os.path.isfile(ctx.obj['power_mat_path']):
+        ctx.obj.update({'power_present':1})
+
         
 
 @main.command()
@@ -38,14 +57,9 @@ def main(ctx):
 @click.pass_context
 def setpath(ctx, path):
         
-    # check if path exists
-    if not os.path.isfile(path):
-        click.secho(f"\n -> '{path}' file was not found.\n", fg = 'yellow', bold = True)
-        return
-    
-    # set path
+    # add path to original settings
     ctx.obj['settings']['index_path'] = path
-        
+    breakpoint()
     # write to file
     with open(settings_path, 'w') as file:
         yaml.dump(ctx.obj['settings'], file)
@@ -56,7 +70,15 @@ def setpath(ctx, path):
 @click.argument('freq_range', type = str)
 # @click.option('--stft', default='1-30', help = 'Run stft analysis')
 @click.pass_context
-def stft(freq_range):
+def stft(ctx, freq_range):
+    
+    # file_present_check(ctx)
+    
+    # check if path exists
+    if ctx.obj['index_present']:
+        click.secho("\n -> Index file was not found.\n", fg = 'yellow', bold = True)
+        return
+    
 
     click.secho(f"\n -> '{freq_range}' was chosen\n" , fg = 'green', bold = True)
     
@@ -81,25 +103,3 @@ if __name__ == '__main__':
     settings_path = 'settings.yaml'
     # start
     main(obj={})
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
