f57339e1fdd1b2143a1a22c4641673a9181d5db0
pantelis <pantelis71@hotmail.com>
2021-08-12 09:41:17 -0400

    move index, add remove mains

--- stft.py
+++ stft.py
@@ -7,11 +7,61 @@ Created on Tue Aug 10 11:11:18 2021
 
 ########## ------------------------------- IMPORTS ------------------------ ##########
 import numpy as np
+import pandas as pd
 from beartype import beartype
 from scipy.signal import stft
 ########## ---------------------------------------------------------------- ##########
 
+class GetIndex():
+    "Get index"
+    
+    def __init__(self, array):
+        self.array = array
 
+    def find_nearest(self, value):
+        """
+        find nearest value in self.array
+
+        Parameters
+        ----------
+        value : values to search the array for
+
+        Returns
+        -------
+        idx for values
+        
+        """
+        return (np.abs(self.array - value)).argmin()
+
+@beartype
+def get_freq_index(freq_vector:np.ndarray, freqs) -> np.ndarray:
+    """
+    Get frequency index
+
+    Parameters
+    ----------
+    freq_vector : np.ndarray, frequency vector to be indexed
+    freqs : values to find the index
+
+    Returns
+    -------
+    np.ndarray
+
+    """
+    
+    # instantiate
+    f = GetIndex(freq_vector)
+    
+    # vectorize function
+    vfunc = np.vectorize(f.find_nearest)
+
+    # get index 
+    return vfunc(freqs)
+
+def pandas_fill(arr:np.ndarray) -> np.ndarray:
+    df = pd.DataFrame(arr)
+    df = df.fillna(method='ffill', axis=0)
+    return  df.values
 
 # Single PSD class
 class Stft:
@@ -69,7 +119,7 @@ class Stft:
         return freq_idx
         
     @beartype
-    def run_stft(self, input_wave:np.ndarray):
+    def get_stft(self, input_wave:np.ndarray):
         """
         Run short time fourier transfrom on input_wave.
 
@@ -88,6 +138,31 @@ class Stft:
         pmat = np.square(np.abs(pmat[self.f_idx[0] : self.f_idx[1]+1,:]))
         
         return f[self.f_idx[0] : self.f_idx[1]+1], pmat
+
+    @staticmethod
+    def remove_mains(freq:np.ndarray, pmat:np.ndarray, f_noise:list):
+
+        # find frequency index
+        f_idx = get_freq_index(freq, f_noise)
+
+        # set noise index to NaNs
+        pmat[f_idx[0]:f_idx[1]+1,:] = np.nan
+
+        # fill NaNs
+        pmat = pandas_fill(pmat)
+
+        return pmat
+
+
+    def run_stft(self, input_wave:np.ndarray, f_noise:list):
+
+        # get stft
+        freq, pmat = self.get_stft(input_wave)
+
+        # remove mains nose
+        pmat = Stft.remove_mains(freq, pmat, f_noise)
+
+        return freq, pmat
         
         
         
