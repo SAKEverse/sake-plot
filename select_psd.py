81715942821451bed1afc83e91c654e716271447
Pantelis Antonoudiou <pantelis71@hotmail.com>
2021-08-18 17:34:34 -0400

    Update select_psd.py

--- select_psd.py
+++ select_psd.py
@@ -19,64 +19,75 @@ class matplotGui(object):
     
     ind = 0 # set internal counter
        
-    def __init__(self, index_df, power_df, n_plot = [4, 1]):
-        """   
-        Parameters
-        ----------
-        data : 3D Numpy array, (1D = seizure segments, 2D =  columns (samples: window*sampling rate), 3D = channels ) 
-        idx_bounds : 2D Numpy array (1D = seizure segments, 2D, 1 = start, 2 = stop index)
-        obj: UserVerify object
-        file_id: Str, file anme
-        """
+    def __init__(self, save_path, index_df, power_df):
+
         
         # pass object attributes to class
+        self.save_path = save_path
         self.power_df = power_df                      
-        self.index_df = index_df 
-        self.nplots = n_plot[0] * n_plot[1]
-        # self.facearray = ['w']*idx_bounds.shape[0]              # color list
+        self.index_df = index_df
+        
+        # set accepted to -1
+        self.index_df['accepted'] = -1
+        
+        # get background
+        self.index_df['facearray'] = 'w'
         
         # create figure and axis
-        self.fig, self.axs = plt.subplots(n_plot[0], n_plot[1], sharex = False, figsize=(8,8))
+        self.fig, self.axs = plt.subplots(1, 1, sharex = False, figsize=(8,8))
 
-        # remove all axes except left 
-        for i in range(self.axs.shape[0]): 
-            self.axs[i].spines["top"].set_visible(False)
-            self.axs[i].spines["right"].set_visible(False)
+        # remove top and right axis
+        self.axs.spines["top"].set_visible(False)
+        self.axs.spines["right"].set_visible(False)
             
         # create first plot
         self.plot_data()
     
     def save_idx(self):
         """
-        Save user predictions to csv file as binary
+        Saves accepted PSD index and mat files
         Returns
         -------
         None.
         """
-        # pre allocate file with zeros
-        ver_pred = np.zeros(self.data.shape[0])
 
-
-        for i in range(self.idx_out.shape[0]): # assign index to 1
-        
-            if self.idx_out[i,0] > 0:
-                # add 1 to stop bound because of python indexing
-                ver_pred[self.idx_out[i,0]:self.idx_out[i,1]+1] = 1
+        # check if all PSDs were verified
+        if np.any(self.index_df['accepted'] == -1) == True:
+            print('Some PSDs were not verified.')
             
-        # save file
-        np.savetxt(os.path.join(self.verpred_path,self.file_id), ver_pred, delimiter=',',fmt='%i')
-        print('Verified predictions for ', self.file_id, ' were saved.\n')    
+        # get accepted PSDs
+        accepted_idx = self.index_df['accepted'] == 1
+        self.index_df = self.index_df[accepted_idx]
+        self.power_df = self.power_df[accepted_idx]
+        
+        # drop extra columns
+        self.index_df = self.index_df.drop(columns = ['accepted','facearray'])
+        
+        # save curated index file
+        self.index_df.to_csv('file_index_accepted.csv')
+        self.power_df.to_pickle('power_mat_accepted.pickle')
+        # save curated power mat file
+
+        print('PSDs were saved.\n')    
         
         
     def get_index(self):
         """
-        get cntr
+
         Returns
         -------
         None.
         """
-
-        self.cnt = self.ind
+        
+        # reset counter when limits are exceeded
+        if self.ind >= len(index_df):
+            self.ind = 0 
+            
+        elif self.ind < 0:
+            self.ind = len(index_df)-1
+       
+        # set counter to internal counter
+        self.i = self.ind
 
         
         
@@ -85,63 +96,25 @@ class matplotGui(object):
         plot_data(self)
         plot self y and t and mark seizure
         """
-        # get index, start and stop times
+        # get index
         self.get_index()
+
+        # clear graph
+        self.axs.clear() 
         
-    
-        # ###  PLot first channel first with different settings  ###   
-        # # get seizure with nearby segments
-        # i = 0  # first channel
-        # y = self.data[self.start - self.seg : self.stop + self.seg,:, i].flatten()
-        # t = np.linspace(self.start - self.seg, self.stop + self.seg, len(y))# get time
-        # self.axs[i].clear() # clear graph
-        # self.axs[i].plot(t, y, color='k', linewidth=0.75, alpha=0.9, label= timestr) 
-        # self.axs[i].set_facecolor(self.facearray[self.i]);
-        # 
-        # self.axs[i].set_title(self.ch_list[i], loc ='left')
+        # get PSD and SEM
+        psd = np.mean(self.power_df['pmat'][self.i], axis = 1)
+        sem = np.std(self.power_df['pmat'][self.i], axis = 1) / np.sqrt(self.power_df['pmat'][self.i].shape[1])
         
-                 
-        # plot remaining channels    
-        for plot_cntr,i in enumerate(range(self.cnt, self.cnt + self.nplots)): 
-            # Plot seizure with surrounding region
-            self.axs[plot_cntr].clear() # clear graph
-            
-            psd = np.mean(self.power_df['pmat'][i], axis = 1)
-            self.axs[plot_cntr].plot(self.power_df['freq'][i], psd, color='gray', linewidth=0.75, alpha=0.9, label = self.index_df.index[i])
-            self.axs[i].legend(loc = 'upper right')
+        # plot new graph
+        self.axs.plot(self.power_df['freq'][self.i], psd, color='black', linewidth=1.5, alpha=0.9, label = self.index_df.index[self.i])
+        self.axs.fill_between(self.power_df['freq'][self.i], psd+sem, psd-sem, color = 'gray')
+        self.axs.set_facecolor(self.index_df['facearray'][self.i]);
+        self.axs.legend(loc = 'upper right')
         
         self.fig.canvas.draw() # draw
 
-
-    ## ------ Mouse Button Press ------ ##   
-    def forward(self, event):
-        self.ind += 1 # add one to class index
-        self.plot_data() # plot
-        
-    def previous(self, event):
-        self.ind -= 1 # subtract one to class index
-        self.plot_data() # plot
-        
-    def accept(self, event):
-        self.facearray[self.i] = 'palegreen'
-        self.axs[0].set_facecolor('palegreen')
-        if self.idx_out[self.i,1] == -1:
-            self.idx_out[self.i,:] = self.idx[self.i,:]
-        else:
-            self.idx_out[self.i,:] = self.idx_out[self.i,:]
-        self.fig.canvas.draw()
-        
-    def reject(self, event):
-        self.facearray[self.i] = 'salmon'
-        self.axs[0].set_facecolor('salmon')
-        self.idx_out[self.i,:] = -1
-        self.fig.canvas.draw()
-        
-    def submit(self, text): # to move to a certain seizure number
-        self.ind = eval(text)
-        self.plot_data() # plot
-
-             
+         
     ## ------  Keyboard press ------ ##     
     def keypress(self,event):
         if event.key == 'right':
@@ -151,21 +124,40 @@ class matplotGui(object):
             self.ind -= 1 # subtract one to class index
             self.plot_data() # plot
         if event.key == 'y':
-            self.facearray[self.i] = 'palegreen'
-            self.axs[0].set_facecolor('palegreen')
-            if self.idx_out[self.i,1] == -1:
-                self.idx_out[self.i,:] = self.idx[self.i,:]
-            else:
-                self.idx_out[self.i,:] = self.idx_out[self.i,:]
-                self.fig.canvas.draw()
+           self.index_df.at[self.i, 'facearray'] = 'palegreen'
+           self.axs.set_facecolor('palegreen')
+           self.index_df.at[self.i, 'accepted'] = 1
+           self.fig.canvas.draw()
         if event.key == 'n':
-            self.facearray[self.i] = 'salmon'
-            self.axs[0].set_facecolor('salmon')
-            self.idx_out[self.i,:] = -1  
+            self.index_df.at[self.i, 'facearray'] = 'salmon'
+            self.axs.set_facecolor('salmon')
+            self.index_df.at[self.i, 'accepted'] = 0
             self.fig.canvas.draw()
-        if event.key == 'enter': 
+        if event.key == 'enter':
             plt.close()
             self.save_idx() # save file to csv
+            
+            
+    #         ## ------ Mouse Button Press ------ ##   
+    # def forward(self, event):
+    #     self.ind += 1 # add one to class index
+    #     self.plot_data() # plot
+        
+    # def previous(self, event):
+    #     self.ind -= 1 # subtract one to class index
+    #     self.plot_data() # plot
+        
+    # def accept(self, event):
+    #     self.facearray[self.i] = 'palegreen'
+    #     self.axs.set_facecolor('palegreen')
+    #     self.index_df['accepted'][self.i] = 1
+    #     self.fig.canvas.draw()
+        
+    # def reject(self, event):
+    #     self.facearray[self.i] = 'salmon'
+    #     self.axs.set_facecolor('salmon')
+    #     self.index_df['accepted'][self.i] = 1
+    #     self.fig.canvas.draw()
 
 
 if __name__ == '__main__':
@@ -191,21 +183,21 @@ if __name__ == '__main__':
     power_df = pd.read_pickle(r'C:\Users\panton01\Desktop\pydsp_analysis\power_mat.pickle')
        
     # init gui object
-    callback = matplotGui(index_df, power_df)
+    callback = matplotGui(parent_folder, index_df, power_df)
     plt.subplots_adjust(bottom=0.15) # create space for buttons
     
     # add title and labels
     callback.fig.suptitle('Select PSDs', fontsize=12)        # title
     callback.fig.text(0.5, 0.09,'Frequency (Hz)', ha="center")                                          # xlabel
     callback.fig.text(.02, .5, 'Power (V^2/Hz)', ha='center', va='center', rotation='vertical')         # ylabel
-    callback.fig.text(0.9, 0.04,'** KEY = Previous : <-, Next: ->, Accept: Y, Reject: N **' ,           # move/accept labels
+    callback.fig.text(0.9, 0.04,'**** KEY ** Previous : <-, Next: ->, Accept: Y, Reject: N ****' ,      # move/accept labels
                       ha="right", bbox=dict(boxstyle="square", ec=(1., 1., 1.), fc=(0.9, 0.9, 0.9),))              
                                                     
     # add key press
     idx_out = callback.fig.canvas.mpl_connect('key_press_event', callback.keypress)
     
     # set useblit True on gtkagg for enhanced performance
-    span = SpanSelector(callback.axs[0], callback.keypress, 'horizontal', useblit=True,
+    span = SpanSelector(callback.axs, callback.keypress, 'horizontal', useblit=True,
         rectprops=dict(alpha=0.5, facecolor='red'))
     plt.show()
 
