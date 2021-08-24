e4559107ea7793aed3d2605961aa03364939ac76
researchgrant <61599897+researchgrant@users.noreply.github.com>
2021-08-24 15:04:33 -0400

    add fast_outliers

--- outlier_detection.py
+++ outlier_detection.py
@@ -9,6 +9,66 @@ import numpy as np
 from numba import njit
 import pandas as pd
 
+def mean_outliers(arr:np.ndarray, window:int, threshold:float) -> np.ndarray:
+    '''
+    Find outliers based on mean, using numpy convolve
+
+    Parameters
+    ----------
+    arr : np.ndarray
+    window : int
+    threshold : float
+
+    Returns
+    -------
+    outliers : np.ndarray
+
+    '''
+    
+    #first make a threshold index:
+        
+    time_thresholds=np.convolve(arr, np.ones(window)/window, mode='valid')
+    
+    #forward fill the terminal values
+    
+    time_thresholds=np.append(time_thresholds,[time_thresholds[-1]]*(len(arr)-len(time_thresholds)))
+    
+    #compare the original array to the threshold index to find the outliers
+    
+    outliers = ((arr>(time_thresholds*threshold)) |  (arr<-(time_thresholds*threshold)))
+    
+    return outliers
+
+def std_outliers(arr:np.ndarray, window:int, threshold:float) -> np.ndarray:
+    '''
+    Find outliers based on standard deviation, using pandas
+
+    Parameters
+    ----------
+    arr : np.ndarray
+    window : int
+    threshold : float
+
+    Returns
+    -------
+    outliers : np.ndarray
+
+    '''
+    
+    #first make a threshold index:
+        
+    time_thresholds=np.array(pd.Series(arr).rolling(window).std().fillna(method='bfill'))
+    time_thresholds=time_thresholds[window//2:]
+    time_thresholds=np.append(time_thresholds,[time_thresholds[-1]]*(len(arr)-len(time_thresholds)))
+    import matplotlib as mpl
+    mpl.pyplot.plot(time_thresholds)
+    #compare the original array to the threshold index to find the outliers
+    
+    outliers = ((arr>(time_thresholds*threshold)) |  (arr<-(time_thresholds*threshold)))
+    
+    return outliers
+
+
 @njit
 def mad(arr):
     return np.median(np.abs(arr-np.median(arr)))
@@ -88,7 +148,7 @@ def rolling_outliers(arr:np.ndarray, window:int, threshold:float) -> np.ndarray:
     return median_value, mad_value
 
 
-def median_outliers(arr:np.ndarray, window:int, threshold:float) -> np.ndarray:
+def fast_outliers(arr:np.ndarray, window:int, threshold:float) -> np.ndarray:
     '''
     finds the outliers based on median by reshaping
 
@@ -106,81 +166,32 @@ def median_outliers(arr:np.ndarray, window:int, threshold:float) -> np.ndarray:
 
     # first make a threshold index:
         
-    # divide the array into windows by reshaping, then find the median of each window, this makes an array of size: original_len/window    
-    medians = pd.DataFrame(arr[:(arr.shape[0]//window)*window].reshape(-1,window)).mad(axis=1)
+    # divide the array into windows by reshaping, then find the median of each window, this makes an array of size: original_len/window
+    reshaped = pd.DataFrame(arr[:(arr.shape[0]//window)*window].reshape(-1,window))
+    medians = reshaped.median(axis=1)
+    
+    mads = reshaped.mad(axis=1)
+
     
     # expand the array back to full size by interpolaing the in between values
     smoothed_medians = np.interp(np.linspace(0, medians.shape[0], medians.shape[0]*window), np.arange(medians.shape[0]), medians)
+    smoothed_mads = np.interp(np.linspace(0, mads.shape[0], mads.shape[0]*window), np.arange(mads.shape[0]), mads)
+    
+    #shift half a window to center the data
+    smoothed_medians = np.append([smoothed_medians[0]]*(window//2),smoothed_medians[:-(window//2)])
+    smoothed_mads= np.append([smoothed_mads[0]]*(window//2),smoothed_mads[:-(window//2)])
     
     # fill the end values 
-    time_thresholds = np.append(smoothed_medians,[smoothed_medians[-1]]*(arr.shape[0]-smoothed_medians.shape[0]))
-    import matplotlib as mpl
-    mpl.pyplot.plot(time_thresholds)
+    smoothed_mads = np.append(smoothed_mads,[smoothed_mads[-1]]*(arr.shape[0]-smoothed_mads.shape[0]))
+    medians = np.append(smoothed_medians,[smoothed_medians[-1]]*(arr.shape[0]-smoothed_medians.shape[0]))
+
     # compare the original array to the threshold index to find the outliers
-    outliers = ((arr>(time_thresholds*threshold)) |  (arr<-(time_thresholds*threshold)))
+    outliers = ((arr>(medians+smoothed_mads*threshold)) |  (arr<(medians-smoothed_mads*threshold)))
     
     return outliers
 
 
-def std_outliers(arr:np.ndarray, window:int, threshold:float) -> np.ndarray:
-    '''
-    Find outliers based on standard deviation, using pandas
 
-    Parameters
-    ----------
-    arr : np.ndarray
-    window : int
-    threshold : float
-
-    Returns
-    -------
-    outliers : np.ndarray
-
-    '''
-    
-    #first make a threshold index:
-        
-    time_thresholds=np.array(pd.Series(arr).rolling(window).std().fillna(method='bfill'))
-    time_thresholds=time_thresholds[window//2:]
-    time_thresholds=np.append(time_thresholds,[time_thresholds[-1]]*(len(arr)-len(time_thresholds)))
-    import matplotlib as mpl
-    mpl.pyplot.plot(time_thresholds)
-    #compare the original array to the threshold index to find the outliers
-    
-    outliers = ((arr>(time_thresholds*threshold)) |  (arr<-(time_thresholds*threshold)))
-    
-    return outliers
-
-
-def mean_outliers(arr:np.ndarray, window:int, threshold:float) -> np.ndarray:
-    '''
-    Find outliers based on mean, using numpy convolve
-
-    Parameters
-    ----------
-    arr : np.ndarray
-    window : int
-    threshold : float
-
-    Returns
-    -------
-    outliers : np.ndarray
-
-    '''
-    
-    #first make a threshold index:
-        
-    time_thresholds=np.convolve(arr, np.ones(window)/window, mode='valid')
-    
-    #forward fill the terminal values
-    
-    time_thresholds=np.append(time_thresholds,[time_thresholds[-1]]*(len(arr)-len(time_thresholds)))
-    
-    #compare the original array to the threshold index to find the outliers
-    
-    outliers = ((arr>(time_thresholds*threshold)) |  (arr<-(time_thresholds*threshold)))
-    
-    return outliers
 
 
 # define function that will be executed
