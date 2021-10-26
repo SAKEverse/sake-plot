36b8b7a8fdbb2a75596a5b6539804692e994aed6
researchgrant <61599897+researchgrant@users.noreply.github.com>
2021-10-26 17:58:08 -0400

    shortcut

--- sakeplot.cmd
+++ sakeplot.cmd
@@ -1,11 +1,30 @@
-call %USERPROFILE%\miniconda3\Scripts\activate.bat
-call %USERPROFILE%\AppData\Local\Continuum\anaconda3\Scripts\activate.bat
+:: Check if Anaconda is present
+IF EXIST %USERPROFILE%\AppData\Local\Continuum\anaconda3\Scripts\activate.bat (
+    set conda_file=%USERPROFILE%\AppData\Local\Continuum\anaconda3
+)
+IF EXIST %USERPROFILE%\anaconda3\Scripts\activate.bat (
+    set conda_file=%USERPROFILE%\anaconda3
+)
+
+:: Check if Miniconda is present
+IF EXIST %USERPROFILE%\miniconda3\Scripts\activate.bat (
+    set conda_file=%USERPROFILE%\miniconda3
+)
+
+:: enter conda
+call %conda_file%\Scripts\activate.bat
+
+:: Navigate sake-plan directory
 cd %USERPROFILE%\Documents\GitHub\sake-plot
-pip install pipenv
-IF EXIST Pipfile.lock (
-    echo "Environment already installed"
-) ELSE (
+
+:: Check if pipenv has been installed
+IF NOT EXIST %conda_file%\Lib\site-packages\pipenv\__main__.py (
+    pip install pipenv
+)
+
+IF NOT EXIST Pipfile.lock (
     pipenv install
 )
+
+:: Launch app
 pipenv run python sakeplot.py
-SLEEP 100
